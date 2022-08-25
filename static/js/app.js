8cabf8e1bd035f5d959bbfad18aadfac14b50f89
SNEHALDD <106944351+SNEHALDD@users.noreply.github.com>
2022-08-25 00:06:43 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -33,10 +33,10 @@ function updateFilters() {
     let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
-    console.log(elementValue);
+    //console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
     let savedId = changedElement.attr("id");
-    console.log(savedId);
+    //console.log(savedId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
@@ -46,7 +46,7 @@ function updateFilters() {
     else {
       delete filters[savedId];
     }
-    console.log(filters);
+    //console.log(filters);
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -67,8 +67,8 @@ function updateFilters() {
     //};
 
     for (elements in filters)
-      console.log(elements);
-      console.log(filters[elements]);
+      //console.log(elements);
+      //console.log(filters[elements]);
 
       filterData = filterData.filter(row => row[elements] === filters[elements]);
     // 10. Finally, rebuild the table using the filtered data
